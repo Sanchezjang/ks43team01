d869b67a2b7ff952a7a7345b7e2c00cfedad6b2d
Ryanghoon <krh3634@naver.com>
2022-06-17 10:05:17 +0900

    인덱스부분 살짝 이름수정과 유저페이지 헤더부분 링크변경했습니다.

--- ks43team01/src/main/java/ks43team01/controller/ReviewController.java
+++ ks43team01/src/main/java/ks43team01/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ks43team01.dto.ReviewContentsReg;
@@ -25,7 +26,14 @@ public class ReviewController {
 	}
 	
 	
-	
+	/* 리뷰 등록 */
+	@PostMapping("/addReview")
+	public String addReview(ReviewContentsReg reviewContentsReg) {
+		
+		reviewService.addReview(reviewContentsReg);
+		
+		return "redirect:/reviewUser/reviewUserList";
+	}
 	
 	
 	/* 리뷰 목록 조회 */
