423b9ff2ffdfac15aed7b67b602aae07542904d7
jangsanghyeok <c2stal@naver.com>
2022-06-21 14:33:03 +0900

    어드민 디폴트 수정

--- ks43team01/src/main/java/ks43team01/mapper/ProfitAndLossMapper.java
+++ ks43team01/src/main/java/ks43team01/mapper/ProfitAndLossMapper.java
@@ -11,25 +11,19 @@ import ks43team01.dto.Spending;
 @Mapper
 public interface ProfitAndLossMapper {
 	//지출 내역 삭제
-	public int removeSpending(String spendingGroupCode);
-	
-	//매출 검색
-	public List<Sales> getSearchSalesList(String searchKey, String searchValue);
-	
-	//지출 검색
-	public List<Spending> getSearchSpendingList(String searchKey, String searchValue);
-	
-	//지출 등록
-	public int addSales(Sales sales);
+	public int removeSpending(Spending spending);
 	
 	//지출 등록
 	public int addSpending(Spending spending);
 	
-	//매출 내역 조회
-	public List<Sales> getSalesList();
+	//매출 등록
+	public int addSales(Sales sales);
 	
 	//지출 내역 조회
 	public List<Spending> getSpendingList();
+	
+	//매출 내역 조회
+	public List<Sales> getSalesList();
 	
 	//계정과목 등록
 	public int addAccount(Account account);
