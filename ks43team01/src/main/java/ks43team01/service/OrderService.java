bd3c7e4b5de0058ec84e3e4874eaa6de6e9d3496
cowonjin <bdnjswls98@naver.com>
2022-06-23 10:59:21 +0900

    orderStatusStandard 기준 조회

--- ks43team01/src/main/java/ks43team01/service/OrderService.java
+++ ks43team01/src/main/java/ks43team01/service/OrderService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ks43team01.dto.Order;
+import ks43team01.dto.OrderCurrentStatus;
 import ks43team01.dto.OrderStatusStandard;
 import ks43team01.dto.QnaBoard;
 import ks43team01.mapper.OrderMapper;
@@ -18,16 +18,22 @@ public class OrderService {
 	public OrderService(OrderMapper orderMapper) {
 			this.orderMapper = orderMapper;
 	}
+	/* 현재 주문 상태 조회 */
+	public List<OrderCurrentStatus> getOrderCurrnetStatusList(){
+		List<OrderCurrentStatus> orderCurrentStatusList = orderMapper.getOrderCurrentStatusList();
+		return orderCurrentStatusList;
+	}
 	
-	/* 상품 주문 */
-	public int addOrder(String sessionId, Order order) {
-		order.setUserIdCode(sessionId);
-		int result = orderMapper.addOrder(order);
+
+	/* 상품 주문 기준 목록 등록 */
+	public int addOrderStatusStandard(String sessionId, OrderStatusStandard orderStatusStandard) {
+		orderStatusStandard.setUserIdCode(sessionId);
+		int result = orderMapper.addOrderStatusStandard(orderStatusStandard);
 		
 		return result;
 	}
 	
-	/* 상품 주문 목록 조회 */
+	/* 상품 주문 기준 목록 조회 */
 	public List<OrderStatusStandard> getOrderStatusStandardList(){
 		List<OrderStatusStandard> orderStatusStandardList = orderMapper.getOrderStatusStandardList();
 		
