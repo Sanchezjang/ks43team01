7e2402500280946e195f8f167116f05cc4aae912
cowonjin <bdnjswls98@naver.com>
2022-06-24 17:25:22 +0900

    20220624 05:24 주문 폼 추가

--- ks43team01/src/main/java/ks43team01/mapper/OrderMapper.java
+++ ks43team01/src/main/java/ks43team01/mapper/OrderMapper.java
@@ -7,12 +7,17 @@ import org.apache.ibatis.annotations.Mapper;
 import ks43team01.dto.Board;
 import ks43team01.dto.OrderCurrentStatus;
 import ks43team01.dto.OrderStatusStandard;
+import ks43team01.dto.Payment;
 
 @Mapper
 public interface OrderMapper {
 	
+	
 	//현재 주문 상태 등록
 	public int addOrderCurrentStatus(OrderCurrentStatus orderCurrentStatus);
+	
+	//현재 주문 상태 상세보기
+	public OrderCurrentStatus getOrderCurrentStatusInfoCode(String orderCode);
 	
 	//현재 주문 상태 조회
 	public List<OrderCurrentStatus> getOrderCurrentStatusList();
