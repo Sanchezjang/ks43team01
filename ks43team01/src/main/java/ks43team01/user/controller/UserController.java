c3d1d973bdcbb4615e1fa67c6613af21a2592267
Sanchezjang <anjdi1004@gmail.com>
2022-06-25 15:37:42 +0900

    중간점검

--- ks43team01/src/main/java/ks43team01/user/controller/UserController.java
+++ ks43team01/src/main/java/ks43team01/user/controller/UserController.java
@@ -4,27 +4,19 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-import javax.xml.ws.http.HTTPBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ks43team01.dto.SellerBusiness;
 import ks43team01.dto.SellerCareer;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import ks43team01.dto.SellerBusiness;
-import ks43team01.dto.SellerCareer;
 import ks43team01.dto.SellerEducation;
 import ks43team01.dto.User;
-import ks43team01.dto.UserLog;
 import ks43team01.dto.GoodsSubCategory;
 import ks43team01.dto.GoodsTopCategory;
 import ks43team01.service.UserService;
