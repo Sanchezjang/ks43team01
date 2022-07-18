58970154c1af483466312043ae1436d96dacb2aa
Sanchezjang <anjdi1004@gmail.com>
2022-07-18 11:18:31 +0900

    수정커밋!!!

--- ks43team01/src/main/java/ks43team01/user/controller/LoginController.java
+++ ks43team01/src/main/java/ks43team01/user/controller/LoginController.java
@@ -4,9 +4,8 @@ package ks43team01.user.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.http.Cookie;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,17 +48,14 @@ public class LoginController {
 	@PostMapping("/login")
 	public String login(@RequestParam(name="userId",required = false)String userId
 						,@RequestParam(name="userPw",required = false)String userPw	
-						,HttpSession session,HttpServletRequest request,UserLog userLog,UserLevelExp userLevelExp,User user, HttpServletResponse response) throws IOException {
-		Cookie cookie = new Cookie("name","sonny");
-		cookie.setMaxAge(60*60);
-		response.addCookie(cookie);
+						,HttpSession session,HttpServletRequest request,UserLog userLog,UserLevelExp userLevelExp,User user, ServletResponse response) throws IOException {
 		User user1 = userService.getUserInfoById(userId);
 		String OS =	System.getProperty("os.version");
 		log.info("운영체제 출력   :   {}",OS);
 		String  userAgent = request.getHeader("User-Agent");
 		log.info("브라우져 기록하기!!!  : {}",userAgent);
 		log.info("user에서 받아온값 :   {}",user1);
-	
+
 		if(user1 != null) {
 			String userPwCheck = user1.getUserPw();
 			if(userPw != null && userPw.equals(userPwCheck)) {
@@ -73,33 +69,15 @@ public class LoginController {
 				userService.addUserLog(userLog, request);
 				userService.addUserLevelExp(userLevelExp);
 				log.info("request값  :  {}",request);
-				
-				String requestUri = request.getRequestURI();
-				String preUri = (String) session.getAttribute("PRE_ADDR");
-				log.info("preUri session 후::: {}", preUri);
-				log.info("session pre session 후::: {}", requestUri);
-				
-				if(preUri != null && !requestUri.equals(preUri)) return "redirect:" + preUri;
-				Cookie cookies = new Cookie("name","sanchez");
-			    ((HttpServletResponse) response).addCookie(cookies);
-			    log.info("--------------cookies쿠키만들어졌는지????!!!  : {}=-----------------",cookies);
-			    log.info("cookies쿠키유효기간 : {}",cookies.getMaxAge());
-			    log.info("cookies/getComment  : {}",cookies.getComment());
-			    log.info("cookies/getDomain  : {}",cookies.getDomain());
-			    log.info("cookies/getName  : {}",cookies.getName());
-			    log.info("cookies/getPath  : {}",cookies.getPath());
-			    log.info("cookies/getValue  : {}",cookies.getValue());
-			    log.info("cookies/getSecure  : {}",cookies.getSecure());
-			    log.info("cookies/getVersion  : {}",cookies.getVersion());
 				return "redirect:/";
+				}
 			}
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('아이디 또는 비밀번호를 확인해주세요~!'+'&'); self.close();</script>");
+				out.flush();
+			return "userpage/user/login";
 		}
-		response.setCharacterEncoding("UTF-8");/*테스트를 위한 작업..궁금했음...*/
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<script>alert('아이디 또는 비밀번호를 확인해주세요~!'+'&'); self.close();</script>");
-		out.flush();
-		return "userpage/user/login";
-	}
 	
-}
+	}
